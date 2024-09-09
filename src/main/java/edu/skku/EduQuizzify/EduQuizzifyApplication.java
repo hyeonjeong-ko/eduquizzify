f7792089cd1692ecd5ced68fcb736e57ca9b6766
hyeonjeong-ko <hyeonkjjang@gmail.com>
2024-09-09 22:32:25 +0900

    Initial commit

---
+++ src/main/java/edu/skku/EduQuizzify/EduQuizzifyApplication.java
@@ -0,0 +1,13 @@
+package edu.skku.EduQuizzify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduQuizzifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduQuizzifyApplication.class, args);
+	}
+
+}
