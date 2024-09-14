26b6481e17168e82bd2c49b4e791c4b2603ddfb9
hyeonjeong-ko <hyeonkjjang@gmail.com>
2024-09-14 11:02:10 +0900

    feat: Add DTOs for quiz request & response

--- src/main/java/edu/skku/EduQuizzify/src/api/dto/QuizRequestDto.java
+++ src/main/java/edu/skku/EduQuizzify/src/api/dto/QuizRequestDto.java
@@ -1,4 +1,11 @@
 package edu.skku.EduQuizzify.src.api.dto;
 
-public class QuizRequestDto {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record QuizRequestDto(
+	@JsonProperty("section_id") String sectionId,
+	@JsonProperty("quiz_type") String quizType,
+	@JsonProperty("focus_prompt") String focusPrompt
+) {
+	// 추가 로직 작성 가능
 }
--- src/main/java/edu/skku/EduQuizzify/src/api/dto/QuizResponseDto.java
+++ src/main/java/edu/skku/EduQuizzify/src/api/dto/QuizResponseDto.java
@@ -1,4 +1,123 @@
 package edu.skku.EduQuizzify.src.api.dto;
 
-public class QuizResponseDTO {
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class QuizResponseDto {
+
+	@JsonProperty("quiz")
+	private Quiz quiz;
+
+	public static class Quiz {
+		@JsonProperty("questions")
+		private List<Question> questions;
+
+		public List<Question> getQuestions() {
+			return questions;
+		}
+
+		public void setQuestions(List<Question> questions) {
+			this.questions = questions;
+		}
+
+		@Override
+		public String toString() {
+			return "Quiz{" +
+				"questions=" + questions +
+				'}';
+		}
+	}
+
+	public static class Question {
+		@JsonProperty("question")
+		private String question;
+
+		@JsonProperty("answers")
+		private List<Answer> answers;
+
+		@JsonProperty("explanation")
+		private String explanation;
+
+		public String getQuestion() {
+			return question;
+		}
+
+		public void setQuestion(String question) {
+			this.question = question;
+		}
+
+		public List<Answer> getAnswers() {
+			return answers;
+		}
+
+		public void setAnswers(List<Answer> answers) {
+			this.answers = answers;
+		}
+
+		public String getExplanation() {
+			return explanation;
+		}
+
+		public void setExplanation(String explanation) {
+			this.explanation = explanation;
+		}
+
+		@Override
+		public String toString() {
+			return "Question{" +
+				"question='" + question + '\'' +
+				", answers=" + answers +
+				", explanation='" + explanation + '\'' +
+				'}';
+		}
+	}
+
+	public static class Answer {
+		@JsonProperty("answer")
+		private String answer;
+
+		@JsonProperty("correct")
+		private boolean correct;
+
+		public String getAnswer() {
+			return answer;
+		}
+
+		public void setAnswer(String answer) {
+			this.answer = answer;
+		}
+
+		public boolean isCorrect() {
+			return correct;
+		}
+
+		public void setCorrect(boolean correct) {
+			this.correct = correct;
+		}
+
+		@Override
+		public String toString() {
+			return "Answer{" +
+				"answer='" + answer + '\'' +
+				", correct=" + correct +
+				'}';
+		}
+	}
+
+	// Getters and Setters for QuizResponseDto
+	public Quiz getQuiz() {
+		return quiz;
+	}
+
+	public void setQuiz(Quiz quiz) {
+		this.quiz = quiz;
+	}
+
+	@Override
+	public String toString() {
+		return "QuizResponseDto{" +
+			"quiz=" + quiz +
+			'}';
+	}
 }
