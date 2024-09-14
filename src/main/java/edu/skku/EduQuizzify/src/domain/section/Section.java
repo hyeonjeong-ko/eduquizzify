011912deb657547253b197f2647abf93f516f487
hyeonjeong-ko <hyeonkjjang@gmail.com>
2024-09-14 11:03:48 +0900

    feat: Add Section domain entities

--- src/main/java/edu/skku/EduQuizzify/src/domain/section/Section.java
+++ src/main/java/edu/skku/EduQuizzify/src/domain/section/Section.java
@@ -1,4 +1,17 @@
 package edu.skku.EduQuizzify.src.domain.section;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Section {
+	private String sectionId;
+	private String processType;
+
+	// 생성자
+	public Section(String sectionId, String processType) {
+		this.sectionId = sectionId;
+		this.processType = processType;
+	}
 }
