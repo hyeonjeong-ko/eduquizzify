6afb958db45f0da301771d63108ce5656da62af7
hyeonjeong-ko <hyeonkjjang@gmail.com>
2024-09-14 10:58:33 +0900

    feat: Add DTO for embedding request

--- src/main/java/edu/skku/EduQuizzify/src/api/dto/EmbeddingRequestDto.java
+++ src/main/java/edu/skku/EduQuizzify/src/api/dto/EmbeddingRequestDto.java
@@ -1,4 +1,22 @@
 package edu.skku.EduQuizzify.src.api.dto;
 
+import lombok.Getter;
+
+@Getter
 public class EmbeddingRequestDto {
+	private String s3_url;
+	private String section_id;
+
+	public EmbeddingRequestDto(String s3_url, String section_id) {
+		this.s3_url = s3_url;
+		this.section_id = section_id;
+	}
+
+	public void setS3_url(String s3_url) {
+		this.s3_url = s3_url;
+	}
+
+	public void setSection_id(String section_id) {
+		this.section_id = section_id;
+	}
 }
